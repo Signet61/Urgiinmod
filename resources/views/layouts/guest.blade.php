86ff2c37a2ede8b84c36f5ee9e5b07896f98c849
B Tengis <tab.erd.tengis@gmail.com>
2026-04-14 15:49:18 +0800

    fix: add pet and login layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <img src="ezgif.com-animated-gif-maker.gif" height="128" width="128" alt="Description of animation">
-                </a>
-            </div>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js',])
+</head>
+
+<script src="js/webPet.min.js"></script>
+<script>
+    window.WebPetLang = {
+        hello: "Сайн уу",
+        welcome: "Тавтай морил",
+        touch: "Яагаад дарна 😄",
+    };
+</script>
+
+
+<script>
+    new WebPet({
+        name: "cat",
+        action: { randomMove: true }
+    });
+</script>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen grid grid-cols-2 sm:justify-center items-center gap-2 pt-6 sm:pt-0 ">
+        <div>
+            <a href="/">
+                <img src="ezgif.com-animated-gif-maker.gif" class="object-cover" alt="Description of animation">
+            </a>
         </div>
-    </body>
+
+        <div
+            class="w-full sm:max-w-md mt-6 px-6 py-4  bg-white shadow-md overflow-hidden sm:rounded-lg justify-self-center mx-auto">
+            <div class="text-center text-3xl">Нэвтрэх</div>
+            {{ $slot }}
+        </div>
+
+    </div>
+</body>
+
 </html>
